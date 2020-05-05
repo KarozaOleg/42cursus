ec3e5bd77229b8ab17c1e2adfc243fc255aa18e6
Oleg <karoza.oleg@gmail.com>
2020-05-05 19:25:48 +0300

    fixed

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-void *memchr(const void *s, int c, size_t n)
+void *ft_memchr(const void *s, int c, size_t n)
 {
     unsigned char   *p;
     size_t          i;
