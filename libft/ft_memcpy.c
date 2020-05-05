4a4e785d043b642c8eaf6873cdad68522759b7d6
Oleg <karoza.oleg@gmail.com>
2020-05-05 19:22:04 +0300

    fixed

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -16,6 +16,9 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
     unsigned char   *p;
     size_t          i;
 
+    if(dest == NULL && src == NULL)
+        return (dest);
+
     i = 0;
     while (i < n)
     {
