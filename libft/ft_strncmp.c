5b8d30a6cf7e06caa5a90913db2744715f794da2
Oleg <karoza.oleg@gmail.com>
2020-05-06 14:51:49 +0300

    fixed

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -18,7 +18,7 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 
     answer = 0;
     i = 0;
-    while (i < n && s1[i] != '\0' && s2[i] != '\0')
+    while (i < n)
     {
         answer = (unsigned char)s1[i] - (unsigned char)s2[i];
         if (answer != 0)
