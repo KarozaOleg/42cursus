34120745a614e4ee82d5bf8483fae0c53eeb9565
Oleg <karoza.oleg@gmail.com>
2020-05-06 13:40:06 +0300

    fixed

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -13,7 +13,7 @@
 
 char *ft_strchr(const char *s, int c)
 {
-    while (*s != '\0')
+    while (*s != '\0' || (char)c == '\0')
     {
         if (*s == (char)c)        
             return ((char *)s);
