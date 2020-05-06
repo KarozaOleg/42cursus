454d6542b6e0553a36c1c7d122609518302bb96e
Oleg <karoza.oleg@gmail.com>
2020-05-06 18:01:23 +0300

    fixed

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -15,7 +15,7 @@ size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
     size_t i;
 
-    if(dst == '\0' || src == '\0')
+    if(*dst == '\0' || *src == '\0')
         return (0);
 
     i = 0;
