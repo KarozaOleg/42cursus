2af949da2f322f903a57c24bf7aefa4a19c6d9cf
Oleg <karoza.oleg@gmail.com>
2020-05-06 12:34:08 +0300

    fixed

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
 void *ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
@@ -20,15 +21,27 @@ void *ft_memccpy(void *dest, const void *src, int c, size_t n)
     i = 0;
     while (i < n)
     {
-        p = &(((unsigned char *)dest)[i]);
-        *p = ((unsigned char *)src)[i];        
+        p = &((unsigned char *)dest)[i];
+        *p = ((unsigned char *)src)[i];  
         ++i;
         if (*p == (unsigned char)c)
         {
             if(i < n)
-                return ((void *)(p++)); 
+                return (++p); 
             break;
         }
     }
     return (NULL);
+}
+
+int main()
+{
+    char src[] = "test basic du memccpy !";
+    char buff1[22];
+
+    printf("%p\n", buff1);
+    memset(buff1, 0, sizeof(buff1));    
+    char *r2 = ft_memccpy(buff1, src, 'm', 22);
+
+    printf("%c\n", r2);
 }
