a4d57a58f6650dba9abfe6ac65d51537c7b8d914
Oleg <karoza.oleg@gmail.com>
2020-05-06 18:00:02 +0300

    fixed

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -15,17 +15,28 @@ size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
     size_t i;
 
+    if(dst == '\0' || src == '\0')
+        return (0);
+
     i = 0;
-    if(size > 0)
+    while (i < size - 1 && src[i] != '\0')
     {
-        while (i < size - 1 && src[i] != '\0')
-        {
-            dst[i] = src[i];
-            ++i;
-        }    
-        dst[i] = '\0';
-        while (src[i] != '\0')
-            ++i;
-    }
+        dst[i] = src[i];
+        ++i;
+    }    
+    dst[i] = '\0';
+    while (src[i] != '\0')
+        ++i;
     return (i);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("Hello is here\n");
+    char *dest = malloc(15 * sizeof(char));
+    size_t answer = ft_strlcpy(dest, "lorem ipsum dolor sit amet", 0);
+    printf("answer: %d\n", answer);
+    return 0;
 }
