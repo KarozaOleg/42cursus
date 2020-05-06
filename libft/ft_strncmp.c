c4d1cef669ae423860674930f5e258aa9fb0a3db
Oleg <karoza.oleg@gmail.com>
2020-05-06 14:48:08 +0300

    feixed

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -18,9 +18,9 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 
     answer = 0;
     i = 0;
-    while (i < n && s1[i] != '\0' && s2[i] != '\0')
+    while (i < n && s1[i] != '\0' && [i] != '\0')
     {
-        answer = s1[i] - s2[i];
+        answer = (unsigned char)s1[i] - (unsigned char)s2[i];
         if (answer != 0)
             break;
         ++i;
