3f5e513538b065539e2ab6ff5f051b3ac9c45367
Oleg <karoza.oleg@gmail.com>
2020-05-06 12:42:40 +0300

    fixed

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -13,7 +13,5 @@
 
 int ft_isalnum(int c)
 {
-    return  ((c >= 65 && c <= 90) ||
-            (c >= 97 && c <= 122) ||
-            (c >= 48 && c <= 57));
+    return (ft_isalpha(c) || ft_isdigit(c));
 }
