6d53eefa93506621234e57037efe402aba028042
karozaoleg <karoza.oleg@gmail.com>
2020-05-14 11:40:36 +0300

    fixed

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mgaston <mgaston@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 14:09:59 by marvin            #+#    #+#             */
-/*   Updated: 2020/05/13 17:22:42 by mgaston          ###   ########.fr       */
+/*   Updated: 2020/05/14 11:34:51 by mgaston          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*p;
 	size_t			i;
 
-	if (dest == NULL && src == NULL)
-		return (dest);
 	i = 0;
 	while (i < n)
 	{
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mgaston <mgaston@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/04 10:23:20 by mgaston           #+#    #+#             */
-/*   Updated: 2020/05/13 17:23:51 by mgaston          ###   ########.fr       */
+/*   Updated: 2020/05/14 11:36:04 by mgaston          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	size_t	shift;
 	size_t	i;
 
-	if (dest == NULL && src == NULL)
-		return (dest);
 	shift = 0;
 	if (dest > src && dest < (void*)(((unsigned char *)src) + n))
 		shift = n - 1;
