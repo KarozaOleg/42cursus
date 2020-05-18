74e5141edf829ea7bf960d62d2727a87bcf114b9
karozaoleg <karoza.oleg@gmail.com>
2020-05-18 19:11:34 +0300

    fixed

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mgaston <mgaston@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 15:00:03 by mgaston           #+#    #+#             */
-/*   Updated: 2020/05/14 14:02:54 by mgaston          ###   ########.fr       */
+/*   Updated: 2020/05/18 19:09:09 by mgaston          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,25 +36,28 @@ static int	return_amount(const char *s, char c)
 	return (amount);
 }
 
-static int	is_error_substr(char **arr, int i_arr)
+static int	is_error_ft_substr(char **arr, int i, int *len)
 {
-	if (arr[i_arr - 1] != NULL)
-	{
-		arr[i_arr] = 0;
+	*len = 0;
+	if (arr[i] != NULL)
 		return (0);
-	}
-	while (--i_arr >= 0)
-		free(arr[i_arr]);
+	while (i >= 0)
+		free(arr[i--]);
+	free(arr);
 	return (1);
 }
 
 static int	is_error_prelaunch(const char *s, char c, char ***p_arr)
 {
+	int amount;
+
 	if (s == NULL)
 		return (1);
-	*p_arr = malloc((return_amount(s, c) + 1) * sizeof(char*));
+	amount = return_amount(s, c) + 1;
+	*p_arr = malloc(amount * sizeof(char*));
 	if (*p_arr == NULL)
 		return (1);
+	(*p_arr)[amount - 1] = NULL;
 	return (0);
 }
 
@@ -75,10 +78,9 @@ char		**ft_split(char const *s, char c)
 		if (s[i] == c || s[i] == 0)
 		{
 			if (len > 0)
-				arr[i_arr++] = ft_substr(s, i - len, len);
-			if (is_error_substr(arr, i_arr))
+				arr[i_arr] = ft_substr(s, i - len, len);
+			if (len > 0 && is_error_ft_substr(arr, i_arr++, &len))
 				return (NULL);
-			len = 0;
 		}
 		else
 			++len;
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mgaston <mgaston@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 14:13:07 by mgaston           #+#    #+#             */
-/*   Updated: 2020/05/18 18:52:47 by mgaston          ###   ########.fr       */
+/*   Updated: 2020/05/18 19:00:24 by mgaston          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 
 	little_len = ft_strlen(little);
-	if(little_len < 1)
+	if (little_len < 1)
 		return ((char*)big);
 	i = 0;
 	while (big[i] != '\0' && i < len)
