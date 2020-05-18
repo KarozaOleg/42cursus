34258957c71b40a2f92f6d33aba2307245b9e0ad
karozaoleg <karoza.oleg@gmail.com>
2020-05-18 16:21:27 +0300

    fixed makefile, ft_strnstr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mgaston <mgaston@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 14:13:07 by mgaston           #+#    #+#             */
-/*   Updated: 2020/05/14 13:28:58 by mgaston          ###   ########.fr       */
+/*   Updated: 2020/05/18 16:08:28 by mgaston          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*dest;
+	size_t	s_len;
 
+	s_len = ft_strlen(s);
 	if (s == NULL)
 		return (NULL);
+	if (start > s_len)
+		len = 0;
+	else if (len > s_len - start)
+		len = s_len - start;
 	dest = malloc((len + 1) * sizeof(char));
 	if (dest != NULL)
-		ft_strlcpy(dest, &s[start], len + 1);
+		ft_strlcpy(dest, &s[start], (len + 1));
 	return (dest);
 }
